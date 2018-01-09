65d8de778354ea9ca6885fa49fd22f14230dc176
BIT-USER <BIT-USER@DESKTOP-81R6F60>
2018-01-09 18:05:21 +0900

    7th, 8th. solved.

--- src/com/javaex/problem07/Account.java
+++ src/com/javaex/problem07/Account.java
@@ -2,11 +2,27 @@ package com.javaex.problem07;
 
 public class Account {
 
-    private String accountNo;
-    private int balance;
-    
-    //생성자 작성
-    
-    //필요한 메소드 작성
+	private String accountNo;
+	private int balance;
+
+	public Account() {
+
+	}
+
+	public Account(String accountNo) {
+		this.accountNo = accountNo;
+	}
+
+	public void deposit(int money) {
+		balance += money;
+	}
+
+	public void withdraw(int money) {
+		balance -= money;
+	}
+
+	public void showBalance() {
+		System.out.println(balance);
+	}
 
 }
